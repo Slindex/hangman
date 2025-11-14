a77cfa50a6c64ddeb6ebba1fa4401a2d3ad76637
Slindex <david.echajaya@hotmail.com>
2025-11-13 22:27:25 -0500

    loader.py file added

---
+++ hangman/loader.py
@@ -0,0 +1,17 @@
+import os
+
+
+BASE_DIR = os.path.dirname(__file__)
+WORDS_PATH = os.path.join(BASE_DIR, "data", "words.txt")
+
+def load_words():
+    try:
+        with open(WORDS_PATH, "r", encoding="utf-8") as f:
+            words = [line.strip() for line in f]
+    except FileNotFoundError:
+        print(f"No se encuentra el archivo words.txt en {os.path.dirname(WORDS_PATH)}")
+        return
+    
+    return words
+
+print(load_words())
